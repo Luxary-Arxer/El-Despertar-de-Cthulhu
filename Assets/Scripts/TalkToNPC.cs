11ce97277d5fee2653d26a38c35ac51ebc307980
davidroman00 <davidro3421@gmail.com>
2025-04-21 16:32:30 +0200

    demo de conversación con cultista implementada

--- Assets/Scripts/TalkToNPC.cs
+++ Assets/Scripts/TalkToNPC.cs
@@ -1,10 +1,28 @@
 
 using UnityEngine;
+using DialogueEditor;
 
 public class TalkToNPC : MonoBehaviour
 {
     [SerializeField]
     GameObject _interactCanvas;
+
+    NPCConversation _currentConversation;
+    void OnEnable()
+    {
+        ConversationManager.OnConversationStarted += ConversationStarted;
+        ConversationManager.OnConversationEnded += ConversationEnded;
+    }
+    void OnDisable()
+    {
+        ConversationManager.OnConversationStarted -= ConversationStarted;
+        ConversationManager.OnConversationEnded -= ConversationEnded;
+    }
+    void Awake()
+    {
+        _currentConversation = GetComponent<NPCConversation>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -17,10 +35,20 @@ public class TalkToNPC : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             _interactCanvas.SetActive(false);
+            ConversationManager.Instance.EndConversation();
         }
     }
+
     public void StartTalkToNPC()
     {
-        Debug.Log("Talking to NPC!");
+        ConversationManager.Instance.StartConversation(_currentConversation);
+    }
+    void ConversationStarted()
+    {
+        Cursor.visible = true;
+    }
+    void ConversationEnded()
+    {
+        Cursor.visible = false;
     }
 }
