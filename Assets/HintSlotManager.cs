6010e026200be419efae90efc8342e7913e411a4
davidroman00 <davidro3421@gmail.com>
2025-05-26 16:51:10 +0200

    cambio UI inventario pistas y comienzo a implementar la funcionalidad

---
+++ Assets/HintSlotManager.cs
@@ -0,0 +1,47 @@
+
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintSlotManager : MonoBehaviour
+{
+[Header("UI elements")]
+    [SerializeField]
+    TextMeshProUGUI _hintDescriptionBox;
+    [SerializeField]
+    TextMeshProUGUI _hintNameBox;
+    [SerializeField]
+    GameObject _hintTextBox;
+
+    string _hintName;
+    string _hintDescription;
+
+    public string HintName { set { _hintName = value; } }
+    public string HintDescription { set { _hintDescription = value; } }
+
+    CharacterAudioManager _characterAudioManager;
+
+    void Awake()
+    {
+        _characterAudioManager = FindFirstObjectByType<CharacterAudioManager>();
+    }
+    public void DisplayTextOnInventory()
+    {
+        _hintTextBox.GetComponent<TextMeshProUGUI>().text = _hintName;
+    }
+    public void DisplayHintDescription()
+    {
+        if (ComponentHasHint())
+        {
+            _hintDescriptionBox.text = _hintDescription;
+            _hintNameBox.text = _hintName;
+
+            _characterAudioManager.PlaySound(_characterAudioManager.AudioClips[2], false, .6f, 1);
+        }
+    }
+    
+    bool ComponentHasHint()
+    {
+        return _hintName != "";
+    }
+}
