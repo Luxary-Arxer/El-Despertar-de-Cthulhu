8f6367bf734fac31c678a3e309122e65d1b70484
davidroman00 <davidro3421@gmail.com>
2025-06-03 21:00:27 +0200

    implemento lo que faltaba de las misiones de la bibliotecaria y euriale

--- Assets/Scripts/NPCs and Missions/Post NPC Talk Scripts/Euriale/LaboratoryGuardianMoves.cs
+++ Assets/Scripts/NPCs and Missions/Post NPC Talk Scripts/Euriale/LaboratoryGuardianMoves.cs
@@ -1,18 +1,37 @@
+
 using System.Collections;
-using System.Collections.Generic;
+using DialogueEditor;
 using UnityEngine;
 
 public class LaboratoryGuardianMoves : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    FadeManager _fadeManager;
+    [SerializeField]
+    Transform _finalPosition;
+    [SerializeField]
+    GameObject _guardianCollider;
+    void OnEnable()
     {
-        
+        ConversationManager.OnConversationEnded += GuardianMovesAway;
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnDisable()
     {
-        
+        ConversationManager.OnConversationEnded -= GuardianMovesAway;
+    }
+    void GuardianMovesAway()
+    {
+        if (QuestManager.ReachedFinalNodeGuardianConversation)
+        {
+            _fadeManager.gameObject.SetActive(true);
+            StartCoroutine(WaitThenOpenDoor());
+        }
+    }
+    IEnumerator WaitThenOpenDoor()
+    {
+        yield return new WaitForSeconds(1.5f);
+        QuestManager.ReachedFinalNodeGuardianConversation = false;
+        transform.position = _finalPosition.position;
+        _guardianCollider.SetActive(false);
     }
 }
